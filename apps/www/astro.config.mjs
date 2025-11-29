2e719b66406cc46c9352230ad8dca8a1c0594bc9
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-11-29 12:26:57 +0200

    chore: add initial project structure

---
+++ apps/www/astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"@/*": "./src",
+				"@/repo/ui/*": "../../../packages/ui/src/*",
+			},
+		},
+	},
+
+	integrations: [react()],
+});
