f2049ecfa3d553330a3e7aec6b6631f4e3e23c54
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-08-04 08:23:07 +0200

    feat(www): add placeholder route layout

---
+++ apps/www/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/index";
+import React from "react";
+
+export default function About() {
+	return (
+		<div>
+			<h1>Welcome to the About Page</h1>
+			<Button>Click Me!</Button>
+		</div>
+	);
+}
---
+++ apps/www/src/app/projects/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/index";
+
+export default function Projects() {
+	return (
+		<div>
+			<h1>Welcome to the Projects page</h1>
+			<Button>Click Me!</Button>
+		</div>
+	);
+}
